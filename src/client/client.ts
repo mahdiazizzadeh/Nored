5ca9ca9a747d65b0dcbce6ca925f35da5bd2eb17
Mahdi <yasermahdiazizzadeh@gmail.com>
2022-04-18 14:13:32 +0430

    queue - decoder - types - client done

--- src/client/client.ts
+++ src/client/client.ts
@@ -1,45 +1,70 @@
 import type { Socket } from "net";
+import { EventEmitter } from "stream";
 import Connect from "../connection/connection";
 import { IconnectioOptions } from "../interface/connection.interface";
+import { Reply } from "../types/callback.type";
+import Decoder from "../util/decoder";
+import Queue from "../util/queue";
 
+const event: EventEmitter = new EventEmitter();
 
-abstract class Client {
-    protected readonly socket: Socket;
+const { convertToWritble } = new Decoder();
+
+class Client {
+    private readonly socket: Socket;
+    private reply: Reply = (error, reply) => { };
+    private readonly queue: Queue<Buffer>;
+    private isPending: boolean = false;
 
     constructor(private readonly connectionOpt?: IconnectioOptions) {
+        this.queue = new Queue<Buffer>();
+
         if (!connectionOpt) connectionOpt = {
             port: 6379, host: "localhost"
         };
         this.socket = new Connect(connectionOpt).getConnection();
-        this.socket.isPending = false;
+
         if (connectionOpt.timeOut) this.socket.setTimeout(connectionOpt.timeOut);
         if (connectionOpt.password) this.authToRedis();
 
-        this.closeConnection();
         this.handleEvents();
     }
 
-
-    private handleEvents() {
-        this.socket.on("error", (err: Error) => console.error(err));
-
-        this.socket.on("timeout", () => {
-            console.info("socket Connection Timeout");
-            this.closeConnection();
+    public sendRquest<T>(buf: Buffer): Promise<T> {
+        return new Promise((resolve, reject) => {
+            if (!this.isPending) {
+                this.socket.write(buf);
+                this.reply = (err, reply) => { err ? reject(err) : resolve(reply) };
+                this.isPending = true;
+            } else {
+                this.queue.add(buf as Buffer);
+            }
         });
-
-        this.socket.on("data", (data: Buffer) => { });
-    }
+    };
 
     private authToRedis() {
-        this.sendRquest(this.connectionOpt?.password as unknown as any);
+        const buf: Buffer = convertToWritble(["AUTH", this.connectionOpt!.password as string]);
+        this.sendRquest<boolean>(buf);
     }
 
     private closeConnection() {
         this.socket.destroy();
     }
 
-    public abstract sendRquest<T>(buf: Buffer): Promise<T>;
+    private handleEvents() {
+        this.socket.on("error", (err: Error) => console.error(err));
+        this.socket.on("timeout", () => {
+            console.info("socket Connection Timeout");
+            this.closeConnection();
+        });
+        this.socket.on("data", (data: Buffer) => {
+            this.reply(false, data.toString());
+            this.isPending = false;
+            if (this.queue.getSize > 0) {
+                this.sendRquest(this.queue.shift() as Buffer);
+            }
+        });
+    }
 }
 
 export default Client;
